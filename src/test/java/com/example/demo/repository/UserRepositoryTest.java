80baf366fb071c87db28f1928a3e950fe42e6242
junseung <junseung736@gmail.com>
2024-09-11 20:22:09 +0900

    섹션 3 테스트 추가하기 - h2를 이용한 repository 테스트

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Sql("/sql/user-repository-test-data.sql")
+@DataJpaTest(showSql = true)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.ACTIVE);
+
+        // then
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("sylvan0212@gmail.com", UserStatus.ACTIVE);
+
+        // then
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("sylvan0212@gmail.com", UserStatus.PENDING);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+}
